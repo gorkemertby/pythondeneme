757125bf7c34d7dec77bf39787ee774475d3c607
gorkemert44 <gorkemert.bb@gmail.com>
2025-03-06 10:47:17 +0300

    Analiz Fonksiyon Duzeltmeleri

--- todolist/analyze/views.py
+++ todolist/analyze/views.py
@@ -14,12 +14,10 @@ def AnalyzeView(request,user_id,exam_id):
         user = User.objects.get(id = user_id)
         exam = Exams.objects.get(eid = exam_id)
         answers = Answers.objects.filter(id = user.id,eid = exam_id)
-        #print(answers)
         username = user.username
         exam_name = exam.title
         selected_option = int(len(answers))
         true_rate = 0 
-        #print(int(answers[0].oid))
         for i in range (0, selected_option - 1 ) :
             if Options.objects.get(oid = int(answers[i].oid.oid)).correctness == "true" : 
                  true_rate += 1 
